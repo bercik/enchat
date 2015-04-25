2af562cba67ecc8808e2fdacd1ea5bcdbed10040
Kamil Drzazga <drzazga888@gmail.com>
2015-04-25 22:56:16 +0200

    Specifying server request and response codes

--- shared/src/main/java/messages/server_request/serverRequest.java
+++ shared/src/main/java/messages/server_request/serverRequest.java
@@ -1,11 +1,26 @@
 package messages.server_request;
 
 /**
+ * Enum-y reprezentują przeważnie daną komendę,
+ * jedynie DISCONNECT nie ma odpowiednika w komendzie
+ * wpisanywanej w linii poleceń.
  * @author mateusz
  * @version 1.0
  */
 public enum serverRequest {
-    JUNK(0), SERVER_OVERLOAD(1), USER_EXIST(2);
+    
+    JUNK(0),
+    CONNECT(1),
+    DISCONNECT(2),
+    REGISTER(3),
+    LOGIN(4),
+    LOGOUT(5),
+    USERS(6),
+    TALK(7),
+    ENDTALK(8),
+    BLACKLIST(9),
+    BLOCK(10),
+    UNBLOCK(11);
 
     serverRequest(int iid) {
         id = iid;
